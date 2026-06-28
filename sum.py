f25241c6fb730d755481663b4c07f75464ddfdfb
karan shinde <shindekarandipak>
2026-06-28 07:18:28 +0530

    complet division code

--- sum.py
+++ sum.py
@@ -1,4 +1,4 @@
 a = int(input("enter a number"))
 b = int(input("enter another numbers"))
-sum = a+b
+sum = a + b 
 print("the sum of",a,"and",b,"is",sum)
---
+++ sum1.py
@@ -0,0 +1,4 @@
+a = int(input("first number: "))
+b = int (input("second numbe: "))
+division = a / b
+print("the division of",a,"and",b, "is ",division)
