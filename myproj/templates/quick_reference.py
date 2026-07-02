e60345d80ac91d7b410049054f5fbc3b670873ef
karan shinde <shindekarandipak>
2026-07-02 21:10:37 +0530

    Day 7: Bootstrap navbar cards table badges

---
+++ myproj/templates/quick_reference.py
@@ -0,0 +1,9 @@
+Layout :
+container - centered content with margins
+row - Horizontal row 
+col-md-4 - Column that takes up 4/12 of the width on medium  screens (3 per row)
+col-md-6 - Column that takes up 6/12 of the width on medium screens (2 per row) 
+NAVBAR :
+navbar - Main container for the navigation bar
+navbar-dark - Dark themed navigation bar
+navbar-brand - Branding section of the navigation bar
